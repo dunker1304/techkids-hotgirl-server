f4792a90dcaba20b4cd53850b143cdd30bac0326
dunker <dunker1304@gmail.com>
2018-10-08 21:36:52 +0700

    server

---
+++ build/precache-manifest.61586b453ff167d9a909d820229a58f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c339c326e84007ab6b78f97deeb6378",
+    "url": "/static/media/logo.2c339c32.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39719f57fd083c864e46",
+    "url": "/static/js/main.39719f57.chunk.js"
+  },
+  {
+    "revision": "d60a68d97d6af5757435",
+    "url": "/static/js/1.d60a68d9.chunk.js"
+  },
+  {
+    "revision": "39719f57fd083c864e46",
+    "url": "/static/css/main.8faff68f.chunk.css"
+  },
+  {
+    "revision": "d60a68d97d6af5757435",
+    "url": "/static/css/1.97e60c57.chunk.css"
+  },
+  {
+    "revision": "441ecbf55dcbc8788c07ac0866885d2a",
+    "url": "/index.html"
+  }
+];
